ce2a5ea454d3e5de26ca0a4883741f541264d0f9
neitomic <neitomic@gmail.com>
2026-07-11 17:57:49 +0700

    fix mdprev version detection

--- Formula/mdprev.rb
+++ Formula/mdprev.rb
@@ -1,14 +1,13 @@
 class Mdprev < Formula
   desc "Fast, lightweight local Markdown preview server"
   homepage "https://github.com/neitomic/mdprev"
-  version "0.1.0"
   depends_on :macos
 
   if Hardware::CPU.arm?
-    url "https://github.com/neitomic/mdprev/releases/download/v#{version}/mdprev-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/neitomic/mdprev/releases/download/v0.1.0/mdprev-aarch64-apple-darwin.tar.gz"
     sha256 "b3b7aa45f80560c44c235930919c693fde704991ce579b885af5b38d80e10f50"
   else
-    url "https://github.com/neitomic/mdprev/releases/download/v#{version}/mdprev-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/neitomic/mdprev/releases/download/v0.1.0/mdprev-x86_64-apple-darwin.tar.gz"
     sha256 "c9ab3c2fd0756904b6732b3de666bd9eff15c52819ef7d7276592b0f98d85b0f"
   end
 
